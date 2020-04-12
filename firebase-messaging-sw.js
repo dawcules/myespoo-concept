f72314499fd0560a041fb8e12b3e92d14729d294
dawcules <48752502+dawcules@users.noreply.github.com>
2020-04-12 17:22:52 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -15,3 +15,17 @@ measurementId: "G-QXEVDNCDP8"
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
+
+messaging.requestPermission().then(function() {
+    getToken(messaging);
+    return messaging.getToken();
+   }).then(function(token){
+   console.log(token);
+   })
+   .catch(function(err) {
+   console.log('Permission denied', err);
+   });
+
+    messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+   });
