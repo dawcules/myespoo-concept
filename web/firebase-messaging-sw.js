77a57b23d00a6160588e5949404229e30cbeaa31
dawcules <ilias.doukas@gmail.com>
2020-04-12 12:13:32 +0300

    move firebase init to idex, register sw in a separetea script

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,16 +1,5 @@
       // Your web app's Firebase configuration
-      var firebaseConfig = {
-        apiKey: "AIzaSyAtHq48TkTxtFgCO_OvyNCS10QFWr2rXBo",
-        authDomain: "cityprog-24974.firebaseapp.com",
-        databaseURL: "https://cityprog-24974.firebaseio.com",
-        projectId: "cityprog-24974",
-        storageBucket: "cityprog-24974.appspot.com",
-        messagingSenderId: "1044301582240",
-        appId: "1:1044301582240:web:d33b1fc254ef328de6dec5",
-        measurementId: "G-QXEVDNCDP8"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+
       const messaging = firebase.messaging();
       messaging.requestPermission().then(function() {
      //getToken(messaging);
