396d8a8da7a026b7f2850a4b5ad98fc020093d7e
dawcules <48752502+dawcules@users.noreply.github.com>
2020-04-12 17:48:11 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -15,3 +15,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.',
+      icon: '/firebase-logo.png'
+    };
+  
+    return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
