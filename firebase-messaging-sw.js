beda44302f747552e17ab1ecdcbb9f42becf67f6
dawcules <48752502+dawcules@users.noreply.github.com>
2020-04-12 17:45:37 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-messaging.js');
 
-var firebaseConfig = {
-apiKey: "AIzaSyAtHq48TkTxtFgCO_OvyNCS10QFWr2rXBo",
-authDomain: "cityprog-24974.firebaseapp.com",
-databaseURL: "https://cityprog-24974.firebaseio.com",
-projectId: "cityprog-24974",
-storageBucket: "cityprog-24974.appspot.com",
-messagingSenderId: "1044301582240",
-appId: "1:1044301582240:web:d33b1fc254ef328de6dec5",
-measurementId: "G-QXEVDNCDP8"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAtHq48TkTxtFgCO_OvyNCS10QFWr2rXBo",
+    authDomain: "cityprog-24974.firebaseapp.com",
+    databaseURL: "https://cityprog-24974.firebaseio.com",
+    projectId: "cityprog-24974",
+    storageBucket: "cityprog-24974.appspot.com",
+    messagingSenderId: "1044301582240",
+    appId: "1:1044301582240:web:d33b1fc254ef328de6dec5",
+    measurementId: "G-QXEVDNCDP8"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
