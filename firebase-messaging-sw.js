fa0901bfa2d45499cdae5543feacf9670859d795
dawcules <48752502+dawcules@users.noreply.github.com>
2020-04-12 16:58:09 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,20 @@
       // Your web app's Firebase configuration
       importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js');
       importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-messaging.js');
-      
+
+      var firebaseConfig = {
+        apiKey: "AIzaSyAtHq48TkTxtFgCO_OvyNCS10QFWr2rXBo",
+        authDomain: "cityprog-24974.firebaseapp.com",
+        databaseURL: "https://cityprog-24974.firebaseio.com",
+        projectId: "cityprog-24974",
+        storageBucket: "cityprog-24974.appspot.com",
+        messagingSenderId: "1044301582240",
+        appId: "1:1044301582240:web:d33b1fc254ef328de6dec5",
+        measurementId: "G-QXEVDNCDP8"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
       const messaging = firebase.messaging();
 
       messaging.requestPermission().then(function() {
