0c49545629918af421c8fde6cb70f67fdbf85488
dawcules <ilias.doukas@gmail.com>
2020-04-19 15:10:27 +0300

    general feed lists help and event cars in home

--- lib/pages/general_feed.dart
+++ lib/pages/general_feed.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:cityprog/widgets/lists/clickable_listview.dart';
 //import 'package:cityprog/strings/community_help_strings.dart';
-import 'package:cityprog/widgets/database_widgets/stream_builder_comhelp.dart';
+import 'package:cityprog/widgets/database_widgets/stream_builder_general.dart';
 
 class GeneralFeed extends StatefulWidget {
   //final String _category;
@@ -22,32 +22,7 @@ class _GeneralFeedState extends State<GeneralFeed> {
       child: Scaffold(
         key: key,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                height: 700,
-                width: 400,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.all(10.0),
-                      child: Text(
-                        'PLACEHOLDER',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 26),
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.all(20.0)),
-                    Expanded(
-                      child: StreamBuilderHelp(myQuery: 'Furniture',),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: StreamBuilderGeneral(),
         ),
       ),
     );
