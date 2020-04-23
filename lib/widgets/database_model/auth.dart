9528938e04a0d50ba84cfeea1a005cd76818237a
Rockronnie <roni.jumpponen@gmail.com>
2020-04-23 12:43:47 +0300

    Login lokalisoitu

--- lib/widgets/database_model/auth.dart
+++ lib/widgets/database_model/auth.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:fb_auth/fb_auth.dart';
 
 class Auth {
+
+  //singleton instance, Firebase web instance 
   Auth._internal();  
   static final Auth _instance = Auth._internal();
   final _app = FbApp(
@@ -11,7 +13,6 @@ class Auth {
     databaseURL: "https://cityprog-24974.firebaseio.com/",
     projectId: "cityprog-24974",
     storageBucket: "gs://cityprog-24974.appspot.com/");
-
   
   AuthBloc _auth;
   StreamSubscription<AuthUser> _userChanged;
@@ -24,7 +25,7 @@ class Auth {
     _auth.close();
     _userChanged?.cancel();
   }
-
+  //setter and getter for the authuser.
   void setUser({AuthUser user}){
     _user = user;
   }
@@ -32,6 +33,7 @@ class Auth {
     return _user;
   }
 
+  //User for initializing the authbloc plugin. adding observer pattern to current user
   void initAuth(){
     _auth = AuthBloc(saveUser: _saveUser, deleteUser: _deleteUser, app: _app);
     final _fbAuth = FBAuth(_app);
@@ -40,24 +42,30 @@ class Auth {
       _auth.add(UpdateUser(user));
     });
   }
+
+  //Login
   void login({email,password}){
       print("Logging in");
         _auth.add(LoginEvent(email, password));
   }
 
+  //Account creation
   void createAccount({email, password, name}){
     print("Creating account");
     _auth.add(CreateAccount(email, password, displayName: name));
   }
 
+  //UID. 
   String getUID(){
     print(_user.toString());
     return _user.uid;
   }
 
-
+  //This is used in the main widget to setup the authbloc for the app.
   AuthBloc getAuthBloc(){
     return _auth;
   }
+
+  //factory instance
   factory Auth() => _instance;
 }
