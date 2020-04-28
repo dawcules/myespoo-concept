bb728921288459b9dbff229f0dfddd109a3a6286
Rockronnie <roni.jumpponen@gmail.com>
2020-04-28 10:24:56 +0300

    webfix

--- lib/widgets/database_widgets/auth_check.dart
+++ lib/widgets/database_widgets/auth_check.dart
@@ -1,8 +1,10 @@
 import 'package:cityprog/model/initialize_profile.dart';
 import 'package:cityprog/model/profile_create.dart';
+import 'package:cityprog/pages/general_feed.dart';
 //import 'package:cityprog/pages/general_feed.dart';
 import 'package:cityprog/pages/login_page.dart';
 import 'package:cityprog/widgets/database_model/auth.dart';
+import 'package:cityprog/widgets/navigation/speech_nav_overlay.dart';
 //import 'package:cityprog/widgets/navigation/speech_nav_overlay.dart';
 import 'package:fb_auth/fb_auth.dart';
 import 'package:flutter/material.dart';
@@ -36,17 +38,15 @@ class AuthCheck extends StatelessWidget {
             ProfileCreate().createProfile(user:_user.uid);
             final initializer = InitializeProfile();
             initializer.getUserData();
-            //return SpeechNavigationOverlay(child: GeneralFeed());
-            return Padding(padding: EdgeInsets.all(0),);
+            SpeechNavigationOverlay(child: GeneralFeed());
           }
           //Not creating so we go here.
           print("Logged in");
-          if (!kIsWeb) {
           final initializer = InitializeProfile();
           initializer.getUserData();
-          //return SpeechNavigationOverlay(child: GeneralFeed());
-          }
-          return Padding(padding: EdgeInsets.all(0),);
+          return SpeechNavigationOverlay(child: GeneralFeed());
+         
+          
         }else{
           //Not logged in so we go to login
            print("Not logged in");
