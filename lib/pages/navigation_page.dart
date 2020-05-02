ed3d5991d0aff50a35e135f407fbc3b30890b3cd
mkayyee <moc.liamg.reversed@gmail.com>
2020-05-02 22:49:08 +0300

    Removed personal duplicate from navigation page

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -133,12 +133,12 @@ class NavigationPage extends StatelessWidget {
           route: "/personal",
           routeName: NavigationStrings.personalToLocalized(),
         ),
-          IconRouteNameRow(
-          heroTag: "nav_voting",
-          icon: Icon(Icons.flag),
-          route: "/voting",
-          routeName: NavigationStrings.personalToLocalized(),
-        ),
+        //   IconRouteNameRow(
+        //   heroTag: "nav_voting",
+        //   icon: Icon(Icons.flag),
+        //   route: "/voting",
+        //   routeName: NavigationStrings.personalToLocalized(),
+        // ),
         Padding(
           padding: EdgeInsets.all(8),
         ),
