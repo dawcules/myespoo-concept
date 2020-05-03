8eea9b1984f5b8a32b65023231933d56c80f2f31
dawcules <ilias.doukas@gmail.com>
2020-05-03 19:05:04 +0300

    help request and sign up UI up to date + localisations

--- lib/pages/community_help_sign_page.dart
+++ lib/pages/community_help_sign_page.dart
@@ -25,50 +25,63 @@ class _CommunityHelpPageState extends State<CommunityHelpPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(10.0),
-                        child: Text(
-                          LocalizedCommunityHelpStrings.volunteerBtnToLocalized(),
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 26),
-                        ),
-                      ),
-                      Image.asset('assets/images/helper.jpg'),
-                      Padding(padding: EdgeInsets.all(20.0)),
-                      Container(
-                        margin: EdgeInsets.all(20.0),
-                        child: Text(
-                            LocalizedCommunityHelpStrings.signDescLocalized(),
-                            style: TextStyle(fontSize: 20)),
-                      ),
-                      if (signed == false)
-                      RaisedButton(
-                        color: AppColor.button.color(),
-                        onPressed: () {
-                          setState(() {
-                            signed = true;                            
-                          });
-                          Database().helpSignUp(Auth().getUID());
-                          showDialog(context: context, child: CitizenPointUpdateDialog(amount: 200, title: Text("We appreciate what you did."),));
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 18,
-                            vertical: 16,
-                          ),
-                          child: Text(
-                            LocalizedCommunityHelpStrings.signUpToLocalized(),
-                            style: TextStyle(
-                              color: AppColor.buttonText.color(),
-                              fontSize: 20,
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(10),
+                      elevation: 8,
+                                        child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.all(10.0),
+                            child: Text(
+                              LocalizedCommunityHelpStrings.volunteerBtnToLocalized(),
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 26),
                             ),
                           ),
-                        ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset('assets/images/helper.jpg')),
+                          ),
+                          Padding(padding: EdgeInsets.all(20.0)),
+                          Container(
+                            margin: EdgeInsets.all(20.0),
+                            child: Text(
+                                LocalizedCommunityHelpStrings.signDescLocalized(),
+                                style: TextStyle(fontSize: 20)),
+                          ),
+                          if (signed == false)
+                          RaisedButton(
+                            color: AppColor.button.color(),
+                            onPressed: () {
+                              setState(() {
+                                signed = true;                            
+                              });
+                              Database().helpSignUp(Auth().getUID());
+                              showDialog(context: context, child: CitizenPointUpdateDialog(amount: 200, title: Text("We appreciate what you did."),));
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 18,
+                                vertical: 16,
+                              ),
+                              child: Text(
+                                LocalizedCommunityHelpStrings.signUpToLocalized(),
+                                style: TextStyle(
+                                  color: AppColor.buttonText.color(),
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.all(10),)
+                        ],
                       ),
-                    ],
+                    ),
                   ),
               ],
             ),
