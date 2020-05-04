cfdef7da43e5925527eebe50a08c350f89903928
Rockronnie <roni.jumpponen@gmail.com>
2020-05-04 17:37:55 +0300

    Revert "Revert "puuh""
    This reverts commit 7bb9023ee7c1099c9bd74fab5fee56992c0482e3.

--- lib/widgets/database_widgets/stream_builder_voting.dart
+++ lib/widgets/database_widgets/stream_builder_voting.dart
@@ -1,5 +1,6 @@
-import 'package:cityprog/styles/color_palette.dart';
+//import 'package:cityprog/styles/color_palette.dart';
 import 'package:cityprog/widgets/lists/votingListTile.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,41 +16,33 @@ class StreamBuilderVoting extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
+    return StreamBuilder(
           stream: myQuery,
           builder: (context, snapshot) {
-            if (!snapshot.hasData) return const Text('Loading..');
-            return VotingListViewBuilder(snapshot.data.documents, voteFor, voteAgainst, hasVoted);
-          }),
-    );
+            if (!snapshot.hasData) return SliverToBoxAdapter(child: Text('Loading..'),);
+            return VotingSliverListViewBuilder(snapshot.data.documents, voteFor, voteAgainst, hasVoted);
+          });
   }
 }
 
 //SWagety. Pistin samaan selkeyden
-class VotingListViewBuilder extends StatelessWidget {
+class VotingSliverListViewBuilder extends StatelessWidget {
   final queryData;
   final Function voteFor;
   final Function voteAgainst;
-  final bool hasVoted;
+  final Function hasVoted;
 
-  VotingListViewBuilder(this.queryData,this.voteFor,this.voteAgainst, this.hasVoted);
+  VotingSliverListViewBuilder(this.queryData,this.voteFor,this.voteAgainst, this.hasVoted);
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-        color: AppColor.background.color(),
-        child: ListView.builder(
-            padding: EdgeInsets.all(16),
-            itemCount: queryData.length,
-            itemBuilder: (BuildContext _context, index) {
-              return Column(
-                children: <Widget>[
-                  _buildListItem(context, queryData[index],voteFor,voteAgainst,hasVoted),
-                  Divider(),
-                ],
+    return SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    return _buildListItem(context, queryData[index],voteFor,voteAgainst,hasVoted);
+                  },
+                  childCount: queryData.length,
+                  ),
               );
-            }));
   }
 
   Widget _buildListItem(BuildContext context, index, voteFor, voteAgainst,hasVoted) {
