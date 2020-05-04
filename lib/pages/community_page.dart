926a83176a42cf3b1eeb2ff8b57f794dc75798a4
dawcules <ilias.doukas@gmail.com>
2020-05-04 17:10:40 +0300

    community_page UI tweaks

--- lib/pages/community_page.dart
+++ lib/pages/community_page.dart
@@ -16,42 +16,59 @@ class CommunityPage extends StatelessWidget {
           height: 1000,
           width: 750,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _communityNavigationButton(
-                  LocalizedWidgetStrings.carpoolToLocalized(),
-                  "/carpool",
-                  context),
-              Padding(
-                padding: EdgeInsets.all(8),
-              ),
-              _communityNavigationButton(
-                  LocalizedWidgetStrings.marketplaceToLocalized(),
-                  "/market",
-                  context),
-              Padding(
-                padding: EdgeInsets.all(8),
-              ),
-              _communityNavigationButton(
-                  LocalizedWidgetStrings.helpServicesToLocalized(),
-                  "/communityHelp",
-                  context),
-              Padding(padding: EdgeInsets.all(8)),
-              _communityNavigationButton(
-                  LocalizedWidgetStrings.premisesToLocalized(),
-                  "/premises",
-                  context),
-              Padding(padding: EdgeInsets.all(8)),
-              _communityNavigationButton(
-                 LocalizedWidgetStrings.voteToLocalized(),
-                  "/voting",
-                  context),
-              Padding(padding: EdgeInsets.all(8)),
-              UniversalRaisedButton(
-                title: LocalizedWidgetStrings.fixReportToLocalized(),
-                fontSize: 24,
-                padding: 8,
-                onPressed: () => showDialog(context: context, child: ReportForm()),
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Material(
+                elevation: 8,
+                borderRadius: BorderRadius.circular(10),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _communityNavigationButton(
+                            LocalizedWidgetStrings.carpoolToLocalized(),
+                            "/carpool",
+                            context),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                      ),
+                      _communityNavigationButton(
+                            LocalizedWidgetStrings.marketplaceToLocalized(),
+                            "/market",
+                            context),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                      ),
+                      _communityNavigationButton(
+                            LocalizedWidgetStrings.helpServicesToLocalized(),
+                            "/communityHelp",
+                            context),
+                      Padding(padding: EdgeInsets.all(8)),
+                      _communityNavigationButton(
+                            LocalizedWidgetStrings.premisesToLocalized(),
+                            "/premises",
+                            context),
+                      Padding(padding: EdgeInsets.all(8)),
+                      _communityNavigationButton(
+                            LocalizedWidgetStrings.voteToLocalized(), "/voting", context),
+                      Padding(padding: EdgeInsets.all(8)),
+                      Container(
+                        width: 260,
+                        height: 60,
+                        child: UniversalRaisedButton(
+                            title: LocalizedWidgetStrings.fixReportToLocalized(),
+                            fontSize: 24,
+                            padding: 8,
+                            onPressed: () =>
+                                showDialog(context: context, child: ReportForm()),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                          ),
               ),
             ],
           ),
@@ -62,21 +79,25 @@ class CommunityPage extends StatelessWidget {
 
   Widget _communityNavigationButton(
       String title, String route, BuildContext context) {
-    return RaisedButton(
-      color: AppColor.button.color(),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          title,
-          style: TextStyle(
-            fontSize: 24,
-            color: AppColor.buttonText.color(),
+    return ButtonTheme(
+      minWidth: 260,
+      height: 60,
+      child: RaisedButton(
+        color: AppColor.button.color(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              color: AppColor.buttonText.color(),
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
         ),
+        onPressed: () => {Navigator.of(context).pushNamed(route)},
       ),
-      onPressed: () => {Navigator.of(context).pushNamed(route)},
     );
   }
 }
